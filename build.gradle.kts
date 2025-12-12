dcd33c8061d389c43c6a8a8ebd6d539f76e401ca
Tianhao-Gu <tgu@anl.gov>
2025-12-12 14:25:04 -0600

    add Sedona jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,7 @@ plugins {
 val hadoopAwsVersion = "3.4.1"
 val deltaSparkVersion = "4.0.0"
 val scalaVersion = "2.13"
-
-
+val sedonaVersion = "1.8.0"
 
 repositories {
     mavenCentral()
@@ -15,6 +14,8 @@ repositories {
 dependencies {
     runtimeOnly("org.apache.hadoop:hadoop-aws:$hadoopAwsVersion")
     runtimeOnly("io.delta:delta-spark_${scalaVersion}:$deltaSparkVersion")
+    // Apache Sedona for geospatial data processing (shaded JAR includes all deps including GeoTools)
+    runtimeOnly("org.apache.sedona:sedona-spark-shaded-4.0_${scalaVersion}:$sedonaVersion")
 }
 
 
